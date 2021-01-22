cdb02f54c4fe1cba022fbc2223fca640ec852200
Vaishnavi <vaishdw2207@gmail.com>
2021-01-22 20:34:57 +0530

    ui: structure landing

---
+++ lib/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Text(
+                  'Fruti',
+                  style: TextStyle(color: Colors.black, fontSize: 40),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
