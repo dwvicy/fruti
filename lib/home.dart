385a5b6d2efcb02b972d3aeb3991df4a8bd52a12
Vaishnavi <vaishdw2207@gmail.com>
2021-01-22 23:18:38 +0530

    ui: cards + bottom nav

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,23 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+
+List<Widget> names = [
+  Container(
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+            begin: Alignment.topCenter, colors: [Colors.red, Colors.black])),
+  ),
+  Container(
+    decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+            begin: Alignment.topCenter, colors: [Colors.red, Colors.black])),
+  ),
+];
 
 class MyHome extends StatefulWidget {
   @override
@@ -7,6 +25,27 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Likes',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: Search',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Profile',
+      style: optionStyle,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,11 +64,67 @@ class _MyHomeState extends State<MyHome> {
               child: Center(
                 child: Text(
                   'Fruti',
-                  style: TextStyle(color: Colors.black, fontSize: 40, fontFamily:'Poppins' ),
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 40,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.bold),
                 ),
               ),
-            )
+            ),
+            Container(
+                child: CarouselSlider(
+              options: CarouselOptions(
+                aspectRatio: 2.0,
+                enlargeCenterPage: true,
+                enableInfiniteScroll: false,
+                initialPage: 2,
+                autoPlay: true,
+              ),
+              items: names,
+            )),
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(color: Colors.white, boxShadow: [
+          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+        ]),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+                gap: 8,
+                activeColor: Colors.white,
+                iconSize: 24,
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                duration: Duration(milliseconds: 800),
+                tabBackgroundColor: Colors.grey[800],
+                tabs: [
+                  GButton(
+                    icon: LineIcons.home,
+                    text: 'Home',
+                  ),
+                  GButton(
+                    icon: LineIcons.arrow_circle_down,
+                    text: 'Likes',
+                  ),
+                  GButton(
+                    icon: LineIcons.search,
+                    text: 'Search',
+                  ),
+                  GButton(
+                    icon: LineIcons.user,
+                    text: 'Profile',
+                  ),
+                ],
+                selectedIndex: _selectedIndex,
+                onTabChange: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                }),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ void main() {
 class Fruti extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: MyHome());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHome(),
+      darkTheme: ThemeData.dark(),
+    );
   }
 }
